821194fec5ebc4c1da6f50baa9dfbfd1bbb99cd6
Pranav from Mark III <pranav.bhawan@gmail.com>
2021-09-20 18:54:49 +0530

    Able to get user_position

--- Tests/Adding Google Map to a page/index.js
+++ Tests/Adding Google Map to a page/index.js
@@ -1,14 +1,56 @@
 // Initialize and add the map
 function initMap() {
   // The location of Uluru
-  var uluru = { lat: 26.2195, lng: 72.94225 };
-  var uluru2 = { lat: 26.2185, lng: 72.94225 };
-
+  var infoWindow = new google.maps.InfoWindow({map: map});
 
   var map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 15,
-    center: uluru,
+    zoom: 16,
+    // center: new google.maps.LatLng(26.2195, 72.94225),
   });
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+        var pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+        };
+
+        infoWindow.setPosition(pos);
+        infoWindow.setContent('Location found.');
+        map.setCenter(pos);
+    }, function() {
+        handleLocationError(true, infoWindow, map.getCenter());
+    });
+
+} else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+                          'Error: The Geolocation service failed.' :
+                          'Error: Your browser doesn\'t support geolocation.');
+}
+
+  // const iconBase =
+  // "https://developers.google.com/maps/documentation/javascript/examples/full/images/";
+
+  var iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
+
+  const icons = {
+    ambulance_ptv: {
+      icon: iconBase + "ambulance_ptv.png",
+    },
+    ambulance_icu: {
+      icon: iconBase + "ambulance_icu.png",
+    },
+    ambulance_super_fast: {
+      icon: iconBase + "ambulance_super_fast.jpeg",
+    }
+    };
+
 
   locations_jdh = [[{lat: 26.2195, lng: 72.94225}],
                [{lat: 26.2175, lng: 72.94225}], 
@@ -17,11 +59,41 @@ function initMap() {
                [{lat: 26.2188, lng: 72.94246}],
                [{lat: 26.2185, lng: 72.94218}]]
   // The map, centered at Uluru
+
+  var features = []
+
+  for(i=0;i<2;i++)
+  {
+    lat_long = locations_jdh[i][0]
+    // console.log(Object.values(lat_long))
+    new_item = {
+      position: new google.maps.LatLng(lat_long),
+      type: "ambulance_super_fast",
+    }
+    features.push(new_item)
+  
+    lat_long = locations_jdh[i+1][0]
+    new_item = {
+      position: new google.maps.LatLng(lat_long),
+      type: "ambulance_icu",
+    }
+    features.push(new_item)
+  
+    lat_long = locations_jdh[i+2][0]
+    new_item = {
+      position: new google.maps.LatLng(lat_long),
+      type: "ambulance_ptv",
+    }
+    features.push(new_item)
+  }
+
+
   for(i=0;i<6;i++)
   {
-    var marker = new google.maps.Marker({
-      position: locations_jdh[i][0],
-      map: map,
+    const marker = new google.maps.Marker({
+      position: features[i].position,
+      // icon: icons.ambulance_super_fast.icon,
+      map:map
     })
   }
   // The marker, positioned at Uluru
