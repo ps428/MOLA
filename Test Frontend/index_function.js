260317d0fee5b58cdd7a57a6a6502a4ce606d21b
blank0826 <aditya26052002@gmail.com>
2021-09-23 23:53:18 +0530

    LogIn

--- Test Frontend/index_function.js
+++ Test Frontend/index_function.js
@@ -1,26 +1,25 @@
 // import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
 function formSubmit() {
-    console.log("hello");
-    // Get Values from the DOM
-  
-    var password = document.querySelector("#password").value;
-    var username = document.querySelector("#username").value;
-    //window.alert("message: " + password + " " + username);
-  
-    //send message values
-    firebase.auth().signInWithEmailAndPassword(username, password)
+  console.log("hello");
+  // Get Values from the DOM
+
+  var password = document.querySelector("#password").value;
+  var username = document.querySelector("#username").value;
+  //window.alert("message: " + password + " " + username);
+
+  //send message values
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(username, password)
     .then((userCredential) => {
-      window.location.href = "../Tests/Adding Google Map to a page/index.html";
+      window.location.href = "../Test Frontend/page3_map_user_view.html";
     })
     .catch((error) => {
       var errorMessage = error.message;
-      window.alert("Not able to log in. Check the Credentials!")
+      window.alert("Not able to log in. Check the Credentials!");
       console.log(errorMessage);
     });
+}
 
-  }
-
-  function registerUser(){
-    
-  }
+function registerUser() {}
